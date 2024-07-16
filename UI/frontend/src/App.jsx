825673345c3f47e4d41105eb9a0f9483dedd760e
Harisha <harisha1014abi@gmail.com>
2024-07-16 15:41:33 +0530

    db

--- UI/frontend/src/App.jsx
+++ UI/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
-// import LoginRegister from './pages/LoginRegister';
+import LoginRegister from './pages/LoginRegister';
 
 function App() {
 
@@ -11,7 +11,7 @@ function App() {
     <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                {/* <Route path="/login&reg" element={<LoginRegister/>} /> */}
+                <Route path="/login&reg" element={<LoginRegister/>} />
                 </Routes>
         </Router>
   )
